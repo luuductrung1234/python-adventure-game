1beaa353c5bfed1f6255c4d77762e857e32aeebb
Trung Luu <luuductrung1234@gmail.com>
2022-11-06 13:20:51 +0700

    jump to boost speed

--- Samples/Snow Boarder/Assets/Scripts/PlayerController.cs
+++ Samples/Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField] float torqueAmount = 1f;
+	[SerializeField] float boostSpeed = 30f;
+	[SerializeField] float baseSpeed = 20f;
 
 	private new Rigidbody2D rigidbody;
+	private SurfaceEffector2D surfaceEffector2D;
 
 	// Start is called before the first frame update
 	internal void Start()
 	{
 		rigidbody = GetComponent<Rigidbody2D>();
+		surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
 	}
 
 	private void FixedUpdate()
+	{
+		RotatePlayer();
+		RespondToBoost();
+	}
+
+	private void RespondToBoost()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			surfaceEffector2D.speed = boostSpeed;
+		}
+		else
+		{
+			surfaceEffector2D.speed = baseSpeed;
+		}
+	}
+
+	private void RotatePlayer()
 	{
 		var torque = (torqueAmount * Mathf.Deg2Rad) * rigidbody.inertia;
 
